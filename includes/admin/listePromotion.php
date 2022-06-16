9b91d6f17d51e7a8000b940594cee738b8f38ab3
sanjoy.wong <sanjoy.wong@gmail.com>
2022-06-16 17:01:37 +0200

    modifié liste php

--- includes/admin/listePromotion.php
+++ includes/admin/listePromotion.php
@@ -7,19 +7,20 @@ spl_autoload_register(function($className){
 $sqlQuery = new Sql();
 $tblQuery = array();
 
-$tblQuery = $sqlQuery->getSelect("select * from salles");
+$tblQuery = $sqlQuery->getSelect("select * from promotions");
 
 ?>
+
 <?php require './includes/admin/header.php'; ?>
-       <!--/Table Liste Professeur-->
+            <!--/Table Liste Professeur-->
             <table>
               <thead>
                 <tr>
-                  <th id="nomTable" colspan="5">Liste des salles</th>
+                  <th id="nomTable" colspan="5">Liste des promotions</th>
                 </tr>
                 <tr>
                   <th colspan="5">
-                    <div class="search">
+                    <div class="container">
                       <div class="search-box">
                          <input type="text" class="search-input" placeholder="Recherche..">
                          <i class="fas fa-search search-button"></i>
@@ -29,8 +30,8 @@ $tblQuery = $sqlQuery->getSelect("select * from salles");
                 
                </div>
               <tr id="titreTable">
-                <th>Salles</th>
-                <th>Caracteristique</th>
+                <th>Promotion</th>
+                <th>Programmes d'enseignemants</th>
                 <th>Voir planning</th>
                 <th>Ecoles</th>
                 <th>Actions</th>
@@ -38,22 +39,21 @@ $tblQuery = $sqlQuery->getSelect("select * from salles");
             </thead>
             <tbody>
             <?php for ($i=0; $i <count($tblQuery) ; $i++) { ?>
-                
-                <td><?=$tblQuery[$i]['nom_salle'] ?></td>
-                <td><?=$tblQuery[$i]['caracteristique'] ?></td>
+              <tr>
+                <td><?=$tblQuery[$i]['nom_promotion'] ?></td>
+                <td>Second general</td>
                 <td>
-                <a href="planningesalle.html">
-                <i class="fa-solid fa-calendar-days fa-2x"></i>
-                </a>
+                  <a href="planningpromotion.php">
+                    <i class="fa-solid fa-calendar-days fa-2x"></i>
+                  </a>
                 </td>
                 <td>Ecole 1</td>
                 <td>
-                <i class="fa-solid fa-pen"></i>
-                <i class="fa-solid fa-trash"></i>
+                  <i class="fa-solid fa-pen"></i>
+                  <i class="fa-solid fa-trash"></i>
                 </td>
-                </tr>
-             <?php } ?>
-      
+              </tr>
+              <?php } ?>
             </tbody>
             <tfoot>
               <tr >
@@ -66,7 +66,7 @@ $tblQuery = $sqlQuery->getSelect("select * from salles");
                     data-max-size="maxSize"
                     data-boundary-links="true"
                   > </div>
-                  <button id="buttonTable" type="button"> Ajouter une salle </button></div>
+                  <button id="buttonTable" type="button"> Ajouter une promotion </button></div>
                 </td>
               </tr>
             </tfoot>
