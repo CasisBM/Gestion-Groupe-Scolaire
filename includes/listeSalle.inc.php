979af5970a526af08318acb4f1c4aa13328eeaa7
sanjoy.wong <sanjoy.wong@gmail.com>
2022-07-01 14:12:16 +0200

    modifier listeSalle

--- includes/listeSalle.inc.php
+++ includes/listeSalle.inc.php
@@ -1,7 +1,7 @@
 <?php
 $sqlQuery = new Sql();
 $tblQuery = array();
-$requete = "select s.nom_salle,s.caracteristique,e.nom_etablissement from salles s join etablissements e on s.id_etablissement = e.id_etablissement";
+$requete = "select s.id_salle,s.nom_salle,s.caracteristique,e.nom_etablissement,e.ville from salles s join etablissements e on s.id_etablissement = e.id_etablissement";
 
 if(!empty($_SESSION['etablissement']))
 {
@@ -23,10 +23,12 @@ $tblQuery = $sqlQuery->lister($requete);
     <tr>
       <th colspan="5">
         <div class="search">
+          <form action="herf=" method="POST">
           <div class="search-box">
             <input type="text" class="search-input" placeholder="Recherche..">
             <i class="fas fa-search search-button"></i>
           </div>
+          </form>
       </th>
     </tr>
 
@@ -45,14 +47,16 @@ $tblQuery = $sqlQuery->lister($requete);
       <td><?= $tblQuery[$i]['nom_salle'] ?></td>
       <td><?= $tblQuery[$i]['caracteristique'] ?></td>
       <td>
+        <form action="" method="$_POST">
         <a href="index.php?page=planningesalle">
-          <i class="fa-solid fa-calendar-days fa-2x"></i>
+          <i name="submit" id="submit" class="fa-solid fa-calendar-days fa-2x"></i>
+          </form>
         </a>
       </td>
-      <td><?=$tblQuery[$i]['nom_etablissement'] ?></td>
+      <td><?=$tblQuery[$i]['ville'] ?></td>
       <td>
-        <a href="index.php?page=editSalle&id=<?= $tblQuery[$i]['id_salle'] ?>" class="btn btn-supp"><i class="fa-solid fa-pen"></i></a>
-        <a href="index.php?page=supp&pg=salle&id=<?= $tblQuery[$i]['id_salle'] ?>" class="btn btn-supp" onclick="return confirm('Etes vous certain de supprimer  cette salle ?')"><i class="fa-solid fa-trash"></i></a>
+        <a href="index.php?page=editSalle&id=<?= $tblQuery[$i]['id_salle'] ?>" ><i class="fa-solid fa-pen"></i></a>
+        <a href="index.php?page=supp&pg=salle&id=<?= $tblQuery[$i]['id_salle'] ?>"  onclick="return confirm('Etes vous certain de supprimer  cette salle ?')"><i class="fa-solid fa-trash"></i></a>
       </td>
       </tr>
     <?php } ?>
