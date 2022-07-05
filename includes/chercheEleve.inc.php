4e5033b85d0fa5194f16cba80675e6802e9c6a3e
sanjoy.wong <sanjoy.wong@gmail.com>
2022-07-05 15:55:19 +0200

    modifier le chercher

--- includes/chercheEleve.inc.php
+++ includes/chercheEleve.inc.php
@@ -2,23 +2,22 @@
 
 //var_dump(isset($_POST['frmcheche']));
 
-if (!isset($_POST['frmcheche'])) 
-     {  
-         include './includes/listeEleve.inc.php';
-        //echo $message;
-    } else {
-   
+if (!isset($_POST['frmcheche'])) {
+    include './includes/listeEleve.inc.php';
+    //echo $message;
+} else {
+
 
     $nom = htmlentities(trim($_POST['nom']));
-    $keyword = '%'.$nom.'%';
-   
+    $keyword = '%' . $nom . '%';
+
     $erreurs = array();
 
     if (mb_strlen($nom) === 0) {
         array_push($erreurs, "Il manque le nom de l'éleve");
     };
-    
-    
+
+
     if (count($erreurs)) {
 
         $messageErreur = "<ul>";
@@ -31,99 +30,92 @@ if (!isset($_POST['frmcheche']))
         echo $messageErreur;
         include './includes/listeEleve.inc.php';
     } else {
-      require './includes/header.php';
+        require './includes/header.php';
 
 
-$sqlQuery = new Sql();
-$requete = "SELECT el.id_eleve, el.prenom, el.nom, et.nom_etablissement, p.nom_promotion FROM eleves el JOIN promotions p 
+        $sqlQuery = new Sql();
+        $requete = "SELECT el.id_eleve, el.prenom, el.nom, et.nom_etablissement, p.nom_promotion FROM eleves el JOIN promotions p 
             ON el.id_promotion = p.id_promotion 
-            JOIN etablissements et ON et.id_etablissement = p.id_etablissement" ;
+            JOIN etablissements et ON et.id_etablissement = p.id_etablissement";
 
-if(!empty($_SESSION['etablissement']))
-{
-  $requete .= " where p.id_etablissement = ".$_SESSION['etablissement'];
-  $requete .= " and (el.nom like '$keyword' or el.prenom like '$keyword');";
-}else $requete .= " where el.nom like '$keyword' or el.prenom like '$keyword';";
+        if (!empty($_SESSION['etablissement'])) {
+            $requete .= " where p.id_etablissement = " . $_SESSION['etablissement'];
+            $requete .= " and (el.nom like '$keyword' or el.prenom like '$keyword');";
+        } else $requete .= " where el.nom like '$keyword' or el.prenom like '$keyword';";
 
-$tblQuery = $sqlQuery->lister($requete);
+        $tblQuery = $sqlQuery->lister($requete);
 ?>
 
-            <!--/Table Liste Eleves-->
-            <table>
-              <thead>
+        <!--/Table Liste Eleves-->
+        <table>
+            <thead>
                 <tr>
-                  <th class="nomTable" colspan="6">Liste des eleves</th>
+                    <th class="nomTable" colspan="6">Liste des eleves</th>
                 </tr>
                 <tr>
-                  <th colspan="6">
-                    <div class="search">
-                    <form action="index.php?page=chercheEleve" method="POST">
-                      <div class="search-box">
-                      <?php  if (count($tblQuery)) { ?> 
-                         <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
-                         <i class="fas fa-search search-button"></i>
-                         <?php   } else { ?>
-                            <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
-                         <i class="fas fa-search search-button"></i>
-                            </div><i>il n'y pas de éleve <?=$nom ?></i>
-                <?php   }     ?>
-                      </div>
-                      <input type="hidden" name="frmcheche" />
-          </form>
-                  </th>
+                    <th colspan="6">
+                        <div class="search">
+                            <form action="index.php?page=chercheEleve" method="POST">
+                                <div class="search-box">
+                                    <?php if (count($tblQuery)) { ?>
+                                        <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
+                                        <i class="fas fa-search search-button"></i>
+                                    <?php   } else { ?>
+                                        <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
+                                        <i class="fas fa-search search-button"></i>
+                                </div>
+                                <div><i>il n'y pas de éleve <?= $nom ?></i></div>
+                            <?php   }     ?>
+                        </div>
+                        <input type="hidden" name="frmcheche" />
+                        </form>
+                    </th>
                 </tr>
-                
-               </div>
-              <tr class="titreTable">
-                <th>Prenom NOM</th>
-                <th>Promotions</th>
-                <th>Voir profil</th>
-                <th>Voir planning</th>
-                <th>Ecoles</th>
-                <th>Actions</th>
-              </tr>
+
+                </div>
+                <tr class="titreTable">
+                    <th>Prenom NOM</th>
+                    <th>Promotions</th>
+                    <th>Voir profil</th>
+                    <th>Voir planning</th>
+                    <th>Ecoles</th>
+                    <th>Actions</th>
+                </tr>
             </thead>
             <tbody>
-            <?php for ($i=0; $i <count($tblQuery) ; $i++) { ?>
-              <tr>
-                <td><?=$tblQuery[$i]['prenom']?><?=' '?><?=$tblQuery[$i]['nom']?></td>
-                <td><?=$tblQuery[$i]['nom_promotion']?></td>
-                <td><i class="fa-solid fa-circle-user fa-2x"></i></td>
-                <td>
-                  <a href="planningprof.html">
-                    <i class="fa-solid fa-calendar-days fa-2x"></i>
-                  </a>
-                </td>
-                <td><?=$tblQuery[$i]['nom_etablissement'] ?></td>
-                <td>
-                  <a href="index.php?page=updateEleve&idEleve=<?= $tblQuery[$i]['id_eleve'] ?>"><i class="fa-solid fa-pen"></i></a>
-                  <a href="index.php?page=supprimer&table=eleves&id=<?= $tblQuery[$i]['id_eleve'] ?>" 
-                  onclick="return confirm('Etes vous certain de supprimer cette salle ?')"><i class="fa-solid fa-trash"></i></a>
-                </td>
-              </tr>
-              <?php } ?>
-             
+                <?php for ($i = 0; $i < count($tblQuery); $i++) { ?>
+                    <tr>
+                        <td><?= $tblQuery[$i]['prenom'] ?><?= ' ' ?><?= $tblQuery[$i]['nom'] ?></td>
+                        <td><?= $tblQuery[$i]['nom_promotion'] ?></td>
+                        <td><i class="fa-solid fa-circle-user fa-2x"></i></td>
+                        <td>
+                            <a href="planningprof.html">
+                                <i class="fa-solid fa-calendar-days fa-2x"></i>
+                            </a>
+                        </td>
+                        <td><?= $tblQuery[$i]['nom_etablissement'] ?></td>
+                        <td>
+                            <a href="index.php?page=updateEleve&idEleve=<?= $tblQuery[$i]['id_eleve'] ?>"><i class="fa-solid fa-pen"></i></a>
+                            <a href="index.php?page=supprimer&table=eleves&id=<?= $tblQuery[$i]['id_eleve'] ?>" onclick="return confirm('Etes vous certain de supprimer cette salle ?')"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                <?php } ?>
+
             </tbody>
             <tfoot>
-              <tr >
-                <td  colspan="6">
-                  <div class="footTable">
-                    <div
-                    data-pagination=""
-                    data-num-pages="numPages()"
-                    data-current-page="currentPage"
-                    data-max-size="maxSize"
-                    data-boundary-links="true"
-                  > </div>
-                  <button class="buttonTable" onclick="location.href='index.php?page=ajouterEleve'" type="button"> Ajouter un eleve </button></div>
-                </td>
-              </tr>
+                <tr>
+                    <td colspan="6">
+                        <div class="footTable">
+                            <div data-pagination="" data-num-pages="numPages()" data-current-page="currentPage" data-max-size="maxSize" data-boundary-links="true"> </div>
+                            <button class="buttonTable" onclick="location.href='index.php?page=ajouterEleve'" type="button"> Ajouter un eleve </button>
+                        </div>
+                    </td>
+                </tr>
             </tfoot>
-            </table> 
-            <?php
+        </table>
+<?php
         // require "listeEleve.php";
     }
-
-} 
+}
 
 ?>
--- includes/chercheEnseignant.inc.php
+++ includes/chercheEnseignant.inc.php
@@ -1,123 +1,117 @@
 <?php
 
 
- if (!isset($_POST['frmcheche'])) 
-     {  
-         include './includes/listeEnseignant.inc.php';
-        //echo $message;
-    } else {
-   
+if (!isset($_POST['frmcheche'])) {
+  include './includes/listeEnseignant.inc.php';
+  //echo $message;
+} else {
 
-    $nom = htmlentities(trim($_POST['nom']));
-    $keyword = '%'.$nom.'%';
-   
-    $erreurs = array();
 
-    if (mb_strlen($nom) === 0) {
-        array_push($erreurs, "Il manque le nom de le professeur!");
-    };
-    
-    
-    if (count($erreurs)) {
+  $nom = htmlentities(trim($_POST['nom']));
+  $keyword = '%' . $nom . '%';
 
-        $messageErreur = "<ul>";
-        for ($i = 0; $i < count($erreurs); $i++) {
-            $messageErreur .= "<li>";
-            $messageErreur .= $erreurs[$i];
-            $messageErreur .= "</li>";
-        }
-        $messageErreur .= "</ul>";
-        echo $messageErreur;
-        include './includes/listeEnseignant.inc.php';
-    } else {
+  $erreurs = array();
 
-      require './includes/header.php';  
-     
+  if (mb_strlen($nom) === 0) {
+    array_push($erreurs, "Il manque le nom de le professeur!");
+  };
 
-$sqlQuery = new Sql();
-$requete = "select id_enseignant,prenom,nom from enseignants where prenom like '$keyword' or nom like '$keyword'";
 
-$tblQuery = $sqlQuery->lister($requete);
+  if (count($erreurs)) {
+
+    $messageErreur = "<ul>";
+    for ($i = 0; $i < count($erreurs); $i++) {
+      $messageErreur .= "<li>";
+      $messageErreur .= $erreurs[$i];
+      $messageErreur .= "</li>";
+    }
+    $messageErreur .= "</ul>";
+    echo $messageErreur;
+    include './includes/listeEnseignant.inc.php';
+  } else {
+
+    require './includes/header.php';
+
+
+    $sqlQuery = new Sql();
+    $requete = "select id_enseignant,prenom,nom from enseignants where prenom like '$keyword' or nom like '$keyword'";
+
+    $tblQuery = $sqlQuery->lister($requete);
 
 
 ?>
-            <!--/Table Liste Professeur-->
-            <table>
-              <thead>
-                <tr>
-                  <th class="nomTable" colspan="5">Liste des enseignants</th>
-                </tr>
-                <tr>
-                  <th colspan="5">
-                    <div class="search">
-                    <form action="index.php?page=chercheEnseignant" method="POST">
-                      <div class="search-box">
-                      <?php  if (count($tblQuery)) { ?> 
-                         <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
-                         <i class="fas fa-search search-button"></i>
-                         <?php   } else { ?>
-                          <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
-                         <i class="fas fa-search search-button"></i>
-                         </div><i>il n'y pas de salle <?=$nom ?></i>
-                <?php   }     ?>
-                      <input type="hidden" name="frmcheche" />
-          </form>
-                  </th>
-                </tr>
-                
-               </div>
-              <tr class="titreTable">
-                <th>Prenom NOM</th>
-                <th>Voir profil</th>                
-                <th>Voir planning</th>
-                <th>Ecoles</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-            <?php for ($i=0; $i <count($tblQuery) ; $i++) { ?>
-              <tr>
-                <td><?=$tblQuery[$i]['prenom']?><?=' '?><?=$tblQuery[$i]['nom']?></td>
-                <td><i class="fa-solid fa-circle-user fa-2x"></i></td>
-                <td>
-                  <a href="">
-                    <i class="fa-solid fa-calendar-days fa-2x"></i>
-                  </a>
-                </td>
-                <td>
-                <a href="index.php?page=listeEtablissement&idProf=<?=$tblQuery[$i]['id_enseignant'] ?>">
-                    <i class="fa-solid fa-school-flag fa-2x"></i>
-                  </a></td>
-                <td>
-                <a href="index.php?page=updateEnseignant&idEnseignant=<?= $tblQuery[$i]['id_enseignant'] ?>"><i class="fa-solid fa-pen"></i></a>
-                <a href="index.php?page=supprimer&table=enseignants&id=<?= $tblQuery[$i]['id_enseignant'] ?>" onclick="return confirm('Voulez vous vraiment supprimer ce prof?')"><i class="fa-solid fa-trash"></i></a> 
-                </td>
-              </tr>
-              <?php } ?>
-             
-            </tbody>
-            <tfoot>
-              <tr >
-                <td  colspan="6">
-                  <div class="footTable">
-                    <div
-                    data-pagination=""
-                    data-num-pages="numPages()"
-                    data-current-page="currentPage"
-                    data-max-size="maxSize"
-                    data-boundary-links="true"
-                  > </div>
-                  <button class="buttonTable" onclick="location.href='index.php?page=ajouterEnseignant'" type="button"> Ajouter un enseignant </button></div>
-                </td>
-              </tr>
-            </tfoot>
-            </table> 
+    <!--/Table Liste Professeur-->
+    <table>
+      <thead>
+        <tr>
+          <th class="nomTable" colspan="5">Liste des enseignants</th>
+        </tr>
+        <tr>
+          <th colspan="5">
+            <div class="search">
+              <form action="index.php?page=chercheEnseignant" method="POST">
+                <div class="search-box">
+                  <?php if (count($tblQuery)) { ?>
+                    <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
+                    <i class="fas fa-search search-button"></i>
+                  <?php   } else { ?>
+                    <input type="text" id="nom" name="nom" class="search-input" placeholder="Recherche..">
+                    <i class="fas fa-search search-button"></i><i>il n'y pas de enseignant <?= $nom ?></i>
+                </div>
+              <?php   }     ?>
+              <input type="hidden" name="frmcheche" />
+              </form>
+          </th>
+        </tr>
 
-            <?php
-        // require "listeEnseignant.inc.php";
-    }
+        </div>
+        <tr class="titreTable">
+          <th>Prenom NOM</th>
+          <th>Voir profil</th>
+          <th>Voir planning</th>
+          <th>Ecoles</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php for ($i = 0; $i < count($tblQuery); $i++) { ?>
+          <tr>
+            <td><?= $tblQuery[$i]['prenom'] ?><?= ' ' ?><?= $tblQuery[$i]['nom'] ?></td>
+            <td><i class="fa-solid fa-circle-user fa-2x"></i></td>
+            <td>
+              <a href="">
+                <i class="fa-solid fa-calendar-days fa-2x"></i>
+              </a>
+            </td>
+            <td>
+              <a href="index.php?page=listeEtablissement&idProf=<?= $tblQuery[$i]['id_enseignant'] ?>">
+                <i class="fa-solid fa-school-flag fa-2x"></i>
+              </a>
+            </td>
+            <td>
+              <a href="index.php?page=updateEnseignant&idEnseignant=<?= $tblQuery[$i]['id_enseignant'] ?>"><i class="fa-solid fa-pen"></i></a>
+              <a href="index.php?page=supprimer&table=enseignants&id=<?= $tblQuery[$i]['id_enseignant'] ?>" onclick="return confirm('Voulez vous vraiment supprimer ce prof?')"><i class="fa-solid fa-trash"></i></a>
+            </td>
+          </tr>
+        <?php } ?>
 
-} 
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="6">
+            <div class="footTable">
+              <div data-pagination="" data-num-pages="numPages()" data-current-page="currentPage" data-max-size="maxSize" data-boundary-links="true"> </div>
+              <button class="buttonTable" onclick="location.href='index.php?page=ajouterEnseignant'" type="button"> Ajouter un enseignant </button>
+            </div>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+
+<?php
+    // require "listeEnseignant.inc.php";
+  }
+}
 
 
 //   displayMessage("!");
